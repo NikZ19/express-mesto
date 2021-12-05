e5af83b781a7b515da1e74fd959f59e554442f04
NikZ <gizm55@yandex.ru>
2021-12-06 02:14:29 +0300

    feat: add loggers

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const { celebrate, Joi, errors } = require('celebrate');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const error = require('./middlewares/error');
+const { requestLogger, errorLogger } = require('./middlewares/loggers');
 
 const { PORT = 3000 } = process.env;
 
@@ -20,6 +21,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(helmet());
+
+app.use(requestLogger);
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
@@ -39,6 +42,8 @@ app.use(auth);
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 app.use('*', require('./routes/notFound'));
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use(error);

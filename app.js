772f774318ed7614d608f32e5f00e92a73fee8ec
NikZ <gizm55@yandex.ru>
2021-12-09 12:56:16 +0300

    fix: cors

--- app.js
+++ app.js
@@ -6,18 +6,13 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const { celebrate, Joi, errors } = require('celebrate');
+const cors = require('cors');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const error = require('./middlewares/error');
 const { requestLogger, errorLogger } = require('./middlewares/loggers');
-// const cors = require('./middlewares/cors');
 
-const allowedCors = [
-  'localhost:3000',
-  'http://localhost:3000',
-  'http://mesto.nikz.nomoredomains.rocks',
-  'https://mesto.nikz.nomoredomains.rocks',
-];
+// const cors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 
@@ -25,33 +20,24 @@ const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
+app.use(cors({
+  origin: [
+    'localhost:3000',
+    'http://localhost:3000',
+    'http://mesto.nikz.nomoredomains.rocks',
+    'https://mesto.nikz.nomoredomains.rocks',
+  ],
+  methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE', 'HEAD'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+  credentials: true,
+}));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(helmet());
 
-// app.use(cors);
 app.use(requestLogger);
-
-app.use((req, res, next) => {
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-
-  const { origin } = req.headers;
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.status(200).send();
-  }
-
-  return next();
-});
 
 app.get('/crash-test', () => {
   setTimeout(() => {
